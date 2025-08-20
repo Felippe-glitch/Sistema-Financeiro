565a91f5883c77452a3ea3da01120f5135080696
Felippe Costa Wernersbach Stein <felps.facul@gmail.com>
2025-08-20 07:43:35 -0300

    starter

---
+++ backend/appfinance/src/main/java/com/example/appfinance/AppfinanceApplication.java
@@ -0,0 +1,13 @@
+package com.example.appfinance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppfinanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppfinanceApplication.class, args);
+	}
+
+}
