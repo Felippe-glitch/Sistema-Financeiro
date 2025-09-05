1afad1bc0774bcfeb96741db37bfb2972cd9f0cc
felippe-steinUVV <felippe.stein@uvv.br>
2025-09-05 09:49:17 -0300

    feat: adiciona repositories básicos

---
+++ backend/appfinance/src/main/java/com/example/appfinance/Repository/UsuarioRepository.java
@@ -0,0 +1,9 @@
+package com.example.appfinance.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.appfinance.Models.Usuario;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {}
