7557e1b7a2851d6b427ad50971dfe9e4404e23b3
felippe-steinUVV <felippe.stein@uvv.br>
2025-09-09 09:30:54 -0300

    feat: adiciona CRUD básico para Duplicata

--- backend/appfinance/src/main/java/com/example/appfinance/Services/DuplicataService.java
+++ backend/appfinance/src/main/java/com/example/appfinance/Services/DuplicataService.java
@@ -3,6 +3,7 @@ package com.example.appfinance.Services;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.example.appfinance.Models.Duplicata;
 import com.example.appfinance.Repository.DuplicataRepository;
@@ -15,8 +16,50 @@ public class DuplicataService {
         this.duplicataRepository = duplicataRepository;
     }
 
+    //CRUD BÁSICO PARA DUPLICATA
+
+    @Transactional
+    public Duplicata createDuplicata(Duplicata duplicata){
+        duplicata.setIdDuplicata((null));
+        duplicata = duplicataRepository.save(duplicata);
+        return duplicata;
+    }
+
+    @Transactional
+    public Duplicata updateDuplicata(Duplicata duplicata){
+        Duplicata newDuplicata = duplicataRepository.findById(duplicata.getIdDuplicata()).orElseThrow(() -> new RuntimeException("Duplicata não encontrado: " + duplicata.getIdDuplicata()));
+
+        newDuplicata.setDataEmissao(duplicata.getDataEmissao());
+        newDuplicata.setDataVencimento(duplicata.getDataVencimento());
+        newDuplicata.setValorDuplicata(duplicata.getValorDuplicata());
+        newDuplicata.setDescricaoDuplicata(duplicata.getDescricaoDuplicata());
+        newDuplicata.setStatusDuplicata(duplicata.getStatusDuplicata());
+        newDuplicata.setEmpresa(duplicata.getEmpresa());
+        newDuplicata.setUsuario(duplicata.getUsuario());
+
+        newDuplicata = duplicataRepository.save(newDuplicata);
+
+        return newDuplicata;
+    }
+
+    @Transactional
+    public Duplicata deleteDuplicata(Long id){
+        try {
+            Duplicata duplicata = duplicataRepository.findById(id).orElseThrow(() -> new RuntimeException("Duplicata não encontrada: " + id));
+            duplicataRepository.deleteById(id);
+            return duplicata;
+        } catch (Exception e) {
+            throw new RuntimeException("Erro ao deletar duplicata: " + e.getMessage());
+        }
+    }
+    
     // LISTAR DUPLICATAS PELO ID DO USUÁRIO
     public List<Duplicata> getDuplicataByUsuarioId(Long usuarioId) {
         return duplicataRepository.findByUsuario_IdUsuario(usuarioId);
+    }
+
+    // LISTAR DUPLICATAS PELO ID DA EMPRESA
+    public List<Duplicata> getDuplicatasByEmpresaId(Long empresaId){
+        return duplicataRepository.findByEmpresa_IdEmpresa(empresaId);
     }
 }
