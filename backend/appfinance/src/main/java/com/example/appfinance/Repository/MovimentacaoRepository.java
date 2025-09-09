7aa2cbbe1c469964bac308acd2040731b70be23d
felippe-steinUVV <felippe.stein@uvv.br>
2025-09-09 10:28:51 -0300

    feat: cria CRUD básico para Movimentações

--- backend/appfinance/src/main/java/com/example/appfinance/Repository/MovimentacaoRepository.java
+++ backend/appfinance/src/main/java/com/example/appfinance/Repository/MovimentacaoRepository.java
@@ -3,7 +3,10 @@ package com.example.appfinance.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.example.appfinance.Models.Duplicata;
 import com.example.appfinance.Models.Movimentacao;
 
 @Repository
-public interface MovimentacaoRepository extends JpaRepository<Movimentacao, Long> {}
+public interface MovimentacaoRepository extends JpaRepository<Movimentacao, Long> {
+    boolean existsByDuplicata(Duplicata duplicata);
+}
