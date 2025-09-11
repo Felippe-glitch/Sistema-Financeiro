612cecc3e3314d53e4cf8cd28632478efeec3175
felippe-steinUVV <felippe.stein@uvv.br>
2025-09-11 09:35:28 -0300

    config: configura autorizações para ambiente de desenvolvimento

--- backend/appfinance/src/main/java/com/example/appfinance/Config/WebSecurityConfig.java
+++ backend/appfinance/src/main/java/com/example/appfinance/Config/WebSecurityConfig.java
@@ -2,10 +2,9 @@ package com.example.appfinance.Config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class WebSecurityConfig {
@@ -13,9 +12,12 @@ public class WebSecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-            .csrf(csrf -> csrf.disable()) 
+            .csrf(csrf -> csrf.disable()) // libera PUT/POST/DELETE sem precisar de CSRF token
             .authorizeHttpRequests(auth -> auth
-                .requestMatchers("/usuario").permitAll() 
+                .requestMatchers(HttpMethod.GET, "/usuario/**").permitAll()
+                .requestMatchers(HttpMethod.POST, "/usuario/**").permitAll()
+                .requestMatchers(HttpMethod.PUT, "/usuario/**").permitAll()
+                .requestMatchers(HttpMethod.DELETE, "/usuario/**").permitAll()
                 .anyRequest().authenticated()
             );
 
